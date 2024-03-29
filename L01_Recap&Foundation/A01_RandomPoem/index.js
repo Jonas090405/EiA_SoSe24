c38dd1130afb16bda32bde7870c04bf2d699302c
Jonas090405 <149697759+Jonas090405@users.noreply.github.com>
2024-03-29 21:49:23 +0100

    little changes in naming and content

--- L01_Recap&Foundation/A01_RandomPoem/index.js
+++ L01_Recap&Foundation/A01_RandomPoem/index.js
@@ -1,31 +1,31 @@
 "use strict";
 //Aufgabe <L01_RandomPoem>
 //Jonas Gissler
-//Matrikel: 275577
-//Datum: 
-//Quellen: Jonas Bausch, Albert Wirz
+//Matrikelnummer: 275577
+//Datum: 29.03.24
+//Quellen: Jonas Bausch, Albert Wirz, ChatGPT
 var RandomVers;
 (function (RandomVers) {
     // Arrays für Subjekte, Verben und Objekten
     RandomVers.Subjekte = ["Robin", "Albert", "Tarik", "Kevin", "Joe", "Robert"];
     RandomVers.Verben = ["erfindet", "liebt", "hasst", "studiert", "mag", "braucht"];
-    RandomVers.Objekte = ["Ideen", "Essen", "Mathe", "Cannabis", "TypeScript",];
+    RandomVers.Objekte = ["Ideen", "Essen", "Mathe", "Cannabis", "TypeScript", "Red Bull"];
     // Funktion getVers
     function getVers() {
         let Vers = ""; // Variable für den Vers
         // Zufällige Subjekte, Verben und Objekte
-        let randomSubjektIndex = (Math.random() * RandomVers.Subjekte.length);
-        let randomVerbIndex = (Math.random() * RandomVers.Verben.length);
-        let randomObjektIndex = (Math.random() * RandomVers.Objekte.length);
+        let randomSubjekt = (Math.random() * RandomVers.Subjekte.length);
+        let randomVerb = (Math.random() * RandomVers.Verben.length);
+        let randomObjekt = (Math.random() * RandomVers.Objekte.length);
         // Zusammenstellen des Verses
-        Vers += RandomVers.Subjekte.splice(randomSubjektIndex, 1)[0];
-        Vers += " " + RandomVers.Verben.splice(randomVerbIndex, 1)[0];
-        Vers += " " + RandomVers.Objekte.splice(randomObjektIndex, 1)[0];
+        Vers += RandomVers.Subjekte.splice(randomSubjekt, 1)[0];
+        Vers += " " + RandomVers.Verben.splice(randomVerb, 1)[0];
+        Vers += " " + RandomVers.Objekte.splice(randomObjekt, 1)[0];
         return Vers;
     }
     RandomVers.getVers = getVers;
 })(RandomVers || (RandomVers = {}));
-// For-Schleife
+// For-Schleife 
 for (let i = RandomVers.Subjekte.length; i >= 1; i--) {
     console.log(i);
     let Vers = RandomVers.getVers();
--- L01_Recap&Foundation/A01_RandomPoem/index.ts
+++ L01_Recap&Foundation/A01_RandomPoem/index.ts
@@ -1,34 +1,34 @@
 //Aufgabe <L01_RandomPoem>
 //Jonas Gissler
-//Matrikel: 275577
-//Datum: 
-//Quellen: Jonas Bausch, Albert Wirz
+//Matrikelnummer: 275577
+//Datum: 29.03.24
+//Quellen: Jonas Bausch, Albert Wirz, ChatGPT
 
 namespace RandomVers {
     // Arrays für Subjekte, Verben und Objekten
     export let Subjekte: string[] = ["Robin", "Albert", "Tarik", "Kevin", "Joe", "Robert"];
     export let Verben: string[] = ["erfindet", "liebt", "hasst", "studiert", "mag", "braucht"];
-    export let Objekte: string[] = ["Ideen", "Essen", "Mathe", "Cannabis", "TypeScript",];
+    export let Objekte: string[] = ["Ideen", "Essen", "Mathe", "Cannabis", "TypeScript","Red Bull"];
     
     // Funktion getVers
     export function getVers(): string {
         let Vers: string = ""; // Variable für den Vers
         
         // Zufällige Subjekte, Verben und Objekte
-        let randomSubjektIndex: number = (Math.random() * Subjekte.length);
-        let randomVerbIndex: number = (Math.random() * Verben.length);
-        let randomObjektIndex: number = (Math.random() * Objekte.length);
+        let randomSubjekt: number = (Math.random() * Subjekte.length);
+        let randomVerb: number = (Math.random() * Verben.length);
+        let randomObjekt: number = (Math.random() * Objekte.length);
         
         // Zusammenstellen des Verses
-        Vers += Subjekte.splice(randomSubjektIndex, 1)[0];
-        Vers += " " + Verben.splice(randomVerbIndex, 1)[0];
-        Vers += " " + Objekte.splice(randomObjektIndex, 1)[0];
+        Vers += Subjekte.splice(randomSubjekt, 1)[0];
+        Vers += " " + Verben.splice(randomVerb, 1)[0];
+        Vers += " " + Objekte.splice(randomObjekt, 1)[0];
         
         return Vers;
     } 
 }
 
-// For-Schleife
+// For-Schleife 
 for (let i = RandomVers.Subjekte.length; i >= 1; i--) {
     console.log(i);
     let Vers = RandomVers.getVers();
